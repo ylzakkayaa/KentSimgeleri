5575a46c17d5b623805be9e06c0aa216b40cb58b
ylzakkayaa <ylzakkayaa@gmail.com>
2022-09-22 23:35:17 +0300

    Initial Commit

---
+++ KentSimgeleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KentSimgeleri
+//
+//  Created by Yeliz Akkaya on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
