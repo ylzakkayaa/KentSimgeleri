61e6f1982defce6405a36047c4ef294052812af5
ylzakkayaa <ylzakkayaa@gmail.com>
2022-10-10 14:58:41 +0300

    first commit

---
+++ KentSimgeleri/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  KentSimgeleri
+//
+//  Created by Yeliz Akkaya on 23.09.2022.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
